40c60a6978bb7790e07b100d59174623b2e0db61
Mathias Mortari <mtmortari@yahoo.de>
2016-09-04 20:03:23 -0300

    ajustes na estrutura da classe utilidade e implementado função heuristica

--- Gomoku/src/function/FuncaoHeuristica.java
+++ Gomoku/src/function/FuncaoHeuristica.java
@@ -2,23 +2,20 @@ package function;
 
 import entity.Tabuleiro;
 
-public class FuncaoUtilidade {
+public class FuncaoHeuristica {
 	
 	
 	public static Long calcularValorTabuleirio(Tabuleiro tab, String pecaJogador, String pecaAdversario){
 		Long sum = 0L;
-		sum += FuncaoUtilidade.calculaValorSequenciaSimples(tab, pecaJogador);
-		sum += FuncaoUtilidade.calculaValorSequenciaDupla(tab, pecaJogador);
-		sum += FuncaoUtilidade.calculaValorSequenciaTripla(tab, pecaJogador);
-		sum += FuncaoUtilidade.calculaValorSequenciaQuadrupla(tab, pecaJogador);
-		sum += FuncaoUtilidade.calculaValorSequenciaQuintupla(tab, pecaJogador);
+		sum += FuncaoHeuristica.calculaValorSequenciaSimples(tab, pecaJogador);
+		sum += FuncaoHeuristica.calculaValorSequenciaDupla(tab, pecaJogador);
+		sum += FuncaoHeuristica.calculaValorSequenciaTripla(tab, pecaJogador);
+		sum += FuncaoHeuristica.calculaValorSequenciaQuadrupla(tab, pecaJogador);
 		
-		
-		sum -= FuncaoUtilidade.calculaValorSequenciaSimples(tab, pecaAdversario);
-		sum -= FuncaoUtilidade.calculaValorSequenciaDupla(tab, pecaAdversario);
-		sum -= FuncaoUtilidade.calculaValorSequenciaTripla(tab, pecaAdversario);
-		sum -= FuncaoUtilidade.calculaValorSequenciaQuadrupla(tab, pecaAdversario);
-		sum -= FuncaoUtilidade.calculaValorSequenciaQuintupla(tab, pecaAdversario);
+		sum -= FuncaoHeuristica.calculaValorSequenciaSimples(tab, pecaAdversario);
+		sum -= FuncaoHeuristica.calculaValorSequenciaDupla(tab, pecaAdversario);
+		sum -= FuncaoHeuristica.calculaValorSequenciaTripla(tab, pecaAdversario);
+		sum -= FuncaoHeuristica.calculaValorSequenciaQuadrupla(tab, pecaAdversario);		
 		return sum;
 	}	
 
@@ -236,113 +233,6 @@ public class FuncaoUtilidade {
 							aux2 = j + 1;
 							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
 								sum += valorSequencia;
-							}
-						}
-						
-					}	
-						
-				}				
-			}			
-		}		
-	return sum;	
-	}
-	
-	private static Long calculaValorSequenciaQuintupla(Tabuleiro tab, String pecaJogador){
-		Integer size = tab.getSize();
-		Long sum = 0L;
-		Integer valorSequencia = 1000000000;
-		int aux1, aux2;  //para controle de index out of bounds e ajuda a buscar o vizinho da peça
-		for(int i = 0; i < size; i++){			
-			for(int j = 0; j < size; j++){
-				if(tab.getEstado()[i][j].equals(pecaJogador)){					
-					//peça vizinha ao lado direito
-					// XXXXX				
-					aux1 = i;
-					aux2 = j + 4;
-					if(aux2 < size &&  tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-						//peças do meio
-						aux2 = j+1;
-						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-							aux2 = j+2;
-							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-								aux2 = j+3;
-								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-									sum += valorSequencia;
-								}
-							}
-							
-						}
-						
-					}
-					
-					//peça vizinha a diagonal para esquerda a baixo
-					//    X
-					//   X
-					//  X
-					// X
-					//X
-					aux1 = i + 4;
-					aux2 = j - 4;
-					if(aux1 < size && aux2 > 0 && tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-						//peças do meio
-						aux1 = i + 1;
-						aux2 = j - 1;
-						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-							aux1 = i + 2;
-							aux2 = j - 2;
-							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-								aux1 = i + 3;
-								aux2 = j - 3;
-								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-									sum += valorSequencia;
-								}
-							}
-							
-						}
-					}
-					
-					//peça vizinha direto abaixo
-					//  X
-					//  X
-					//  X
-					//  X
-					//  X
-					aux1 = i + 4;
-					aux2 = j;
-					if(aux1 < size && tab.getEstado()[aux1][aux2].equals(pecaJogador)){						
-						//peças do meio
-						aux1 = i + 1;						
-						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-							aux1 = i + 2;						
-							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-								aux1 = i + 3;						
-								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-									sum += valorSequencia;
-								}
-							}
-						}						
-					}
-					
-					//peça vizinha  a diagonal a direita abaixo
-					//  X
-					//   X
-					//    X
-					//     X
-					//      X
-					aux1 = i + 4;
-					aux2 = j + 4;
-					if(aux1 < size && aux2 < size && tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-						aux1 = i + 1;
-						aux2 = j + 1;
-						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-							aux1 = i + 2;
-							aux2 = j + 2;
-							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-								aux1 = i + 3;
-								aux2 = j + 3;
-								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
-									sum += valorSequencia;
-								}
 							}
 						}
 						
--- Gomoku/src/test/Teste.java
+++ Gomoku/src/test/Teste.java
@@ -16,56 +16,36 @@ public class Teste {
 		
 		
 //		GUI.showTabuleiro(tab);
-	
 		
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 7, 7);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 7, 8);
-		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 7, 9);
+		tab.colocaPecaDoJogadorNoTabuleirio(player2.getValorPeca(), 7, 9);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 7, 10);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 7, 11);
 		
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 8, 6);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 8, 7);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 8, 8);
-		
-		
+				
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 9, 5);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 9, 7);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 9, 9);
-		
-		
+				
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 10, 4);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 10, 7);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 10, 10);
 		
-		
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 11, 3);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 11, 7);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 11, 11);
-		
-		
 		
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 14, 0);
 		tab.colocaPecaDoJogadorNoTabuleirio(player.getValorPeca(), 14, 14);
 		
 	
 		GUI.showTabuleiro(tab);
-		
-		Long simples1 = FuncaoUtilidade.calculaValorSequenciaSimples(tab, player.getValorPeca());
-		System.out.println("simples player 1 " + simples1);
-		
-		
-		Long dupla1 = FuncaoUtilidade.calculaValorSequenciaDupla(tab, player.getValorPeca());
-		System.out.println("dupla player 1 " + dupla1);
-		
-		Long tripla1 = FuncaoUtilidade.calculaValorSequenciaTripla(tab, player.getValorPeca());
-		System.out.println("tripla player 1 " + tripla1);
-		
-		Long quadrupla1 = FuncaoUtilidade.calculaValorSequenciaQuadrupla(tab, player.getValorPeca());
-		System.out.println("quadrupla player 1 " + quadrupla1);
-		
-		Long quintupla1 = FuncaoUtilidade.calculaValorSequenciaQuintupla(tab, player.getValorPeca());
-		System.out.println("quintupla player 1 " + quintupla1);
+		Long valor = FuncaoUtilidade.calcularValorTabuleirio(tab, player.getValorPeca(), player2.getValorPeca());
+		System.out.println("valor tab " + valor);
 		
 		
 	}
