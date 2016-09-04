1c92bc59176c6c34450d85b0a1d6030e8b07b73a
Mathias Mortari <mtmortari@yahoo.de>
2016-09-04 20:24:00 -0300

    adicionado função para verificar se existe quintupla no tabuleiro

--- Gomoku/src/function/FuncaoUtilidade.java
+++ Gomoku/src/function/FuncaoUtilidade.java
@@ -353,4 +353,111 @@ public class FuncaoUtilidade {
 		}		
 	return sum;	
 	}
+	
+	private static Boolean existeQuitupla(Tabuleiro tab, String pecaJogador){
+		Integer size = tab.getSize();
+		
+		int aux1, aux2;  //para controle de index out of bounds e ajuda a buscar o vizinho da peça
+		for(int i = 0; i < size; i++){			
+			for(int j = 0; j < size; j++){
+				if(tab.getEstado()[i][j].equals(pecaJogador)){					
+					//peça vizinha ao lado direito
+					// XXXXX				
+					aux1 = i;
+					aux2 = j + 4;
+					if(aux2 < size &&  tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+						//peças do meio
+						aux2 = j+1;
+						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+							aux2 = j+2;
+							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+								aux2 = j+3;
+								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+									return true;
+								}
+							}
+							
+						}
+						
+					}
+					
+					//peça vizinha a diagonal para esquerda a baixo
+					//    X
+					//   X
+					//  X
+					// X
+					//X
+					aux1 = i + 4;
+					aux2 = j - 4;
+					if(aux1 < size && aux2 > 0 && tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+						//peças do meio
+						aux1 = i + 1;
+						aux2 = j - 1;
+						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+							aux1 = i + 2;
+							aux2 = j - 2;
+							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+								aux1 = i + 3;
+								aux2 = j - 3;
+								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+									return true;
+								}
+							}
+							
+						}
+					}
+					
+					//peça vizinha direto abaixo
+					//  X
+					//  X
+					//  X
+					//  X
+					//  X
+					aux1 = i + 4;
+					aux2 = j;
+					if(aux1 < size && tab.getEstado()[aux1][aux2].equals(pecaJogador)){						
+						//peças do meio
+						aux1 = i + 1;						
+						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+							aux1 = i + 2;						
+							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+								aux1 = i + 3;						
+								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+									return true;
+								}
+							}
+						}						
+					}
+					
+					//peça vizinha  a diagonal a direita abaixo
+					//  X
+					//   X
+					//    X
+					//     X
+					//      X
+					aux1 = i + 4;
+					aux2 = j + 4;
+					if(aux1 < size && aux2 < size && tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+						aux1 = i + 1;
+						aux2 = j + 1;
+						if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+							aux1 = i + 2;
+							aux2 = j + 2;
+							if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+								aux1 = i + 3;
+								aux2 = j + 3;
+								if(tab.getEstado()[aux1][aux2].equals(pecaJogador)){
+									return true;
+								}
+							}
+						}
+						
+					}	
+						
+				}				
+			}			
+		}		
+	return false;
+	}
+	
 }
